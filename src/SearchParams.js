41636ce47e9bd3fc760723fa5ad5989896e9aa14
Mokhtar Ali <46555838+Joemokhtarali@users.noreply.github.com>
2021-08-13 15:33:44 -0400

    adding a form for animal and breed options using Select tag

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
 
 const SearchParams = () => {
+  const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+  const BREEDS = ["bird", "cat", "dog", "rabbit", "reptile"];
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+
   return (
     <div className="search-params">
       <form>
@@ -13,6 +18,34 @@ const SearchParams = () => {
             value={location}
             placeholder="Location"
           />
+        </label>
+        <label htmlFor="animal">
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal} value="animal">
+                {animal}
+              </option>
+            ))}
+          </select>
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+          >
+            <option />
+            {BREEDS.map((breed) => (
+              <option key={breed} value="breed">
+                {breed}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
