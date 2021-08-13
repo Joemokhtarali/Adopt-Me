8cf8552e1bfd2d0f6b83f7a942c16111b8e75163
Mokhtar Ali <46555838+Joemokhtarali@users.noreply.github.com>
2021-08-13 15:14:46 -0400

    UseState Hook

--- src/App.js
+++ src/App.js
@@ -1,37 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
+import SearcParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Sudo" animal="Dog" breed="Wheaten Terrier" />
+      <SearcParams />
     </div>
   );
 };
-// const App = () => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", { id: "my-brand" }, "Adopt Me"),
-//     React.createElement(Pet, {
-//       name: "Luna",
-//       animal: "Dog",
-//       breed: "Havanese",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Pepper",
-//       animal: "Bird",
-//       breed: "Cockatiel",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Sudo",
-//       animal: "Dog",
-//       breed: "Wheaten Terrier",
-//     }),
-//   ]);
-// };
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
