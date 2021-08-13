55b9895ca7bb5c78cd7f021b5e79a374d4bf41b8
Mokhtar Ali <46555838+Joemokhtarali@users.noreply.github.com>
2021-08-13 14:40:53 -0400

    Added Eslint plugins to read React

--- src/App.js
+++ src/App.js
@@ -3,27 +3,48 @@ import ReactDOM from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "my-brand" }, "Adopt Me"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Sudo",
-      animal: "Dog",
-      breed: "Wheaten Terrier",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Sudo" animal="Dog" breed="Wheaten Terrier" />
+    </div>
+  );
 };
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", { id: "my-brand" }, "Adopt Me"),
+//     React.createElement(Pet, {
+//       name: "Luna",
+//       animal: "Dog",
+//       breed: "Havanese",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Pepper",
+//       animal: "Bird",
+//       breed: "Cockatiel",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Sudo",
+//       animal: "Dog",
+//       breed: "Wheaten Terrier",
+//     }),
+//   ]);
+// };
 
-ReactDOM.render(
-  React.createElement(App, {}, null),
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// const animals = [
+//   { name: "Luna", animal: "Dog", breed: "Havanese" },
+//   {
+//     name: "Pepper",
+//     animal: "Bird",
+//     breed: "Cockatiel",
+//   },
+//   {
+//     name: "Sudo",
+//     animal: "Dog",
+//     breed: "Wheaten Terrier",
+//   },
+// ];
